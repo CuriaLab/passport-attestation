96e5d9f7ebe45060a8ad60869c431a89de9b7f7a
Pakorn Nathong <pakorn.nth@hotmail.com>
2024-06-10 14:47:41 +0700

    feat(contract): add custom resolvers

--- contracts/src/AttestationResolver.sol
+++ contracts/src/AttestationResolver.sol
@@ -3,19 +3,58 @@ pragma solidity ^0.8.26;
 
 import "eas-contracts/resolver/SchemaResolver.sol";
 import "eas-contracts/IEAS.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AttestationResolver is SchemaResolver {
-    constructor(IEAS eas) SchemaResolver(eas) {}
+import "./Schema.sol";
+import "./resolvers/ICustomResolver.sol";
 
-    function attest(
-        Attestation calldata attestation
-    ) external payable override returns (bool) {
-        return false;
+contract AttestationResolver is SchemaResolver, Ownable {
+    mapping(address => bool) public authorizedAttesters;
+    mapping(uint256 => ICustomResolver) public customResolvers;
+
+    constructor(IEAS eas) SchemaResolver(eas) Ownable(msg.sender) {}
+
+    error InvalidRole();
+
+    function addAuthorizedAttester(address attester) external onlyOwner {
+        authorizedAttesters[attester] = true;
     }
 
-    function revoke(
-        Attestation calldata attestation
-    ) external payable override returns (bool) {
+    function removeAuthorizedAttester(address attester) external onlyOwner {
+        authorizedAttesters[attester] = false;
+    }
+
+    function setCustomResolver(
+        uint256 role,
+        ICustomResolver resolver
+    ) external onlyOwner {
+        customResolvers[role] = resolver;
+    }
+
+    function onAttest(
+        Attestation calldata attestation,
+        uint256
+    ) internal virtual override returns (bool) {
+        Schema memory schema = abi.decode(attestation.data, (Schema));
+
+        // Check if the attester is authorized -> short circuit
+        if (authorizedAttesters[attestation.attester]) {
+            return true;
+        }
+
+        ICustomResolver resolver = customResolvers[schema.role];
+
+        if (address(resolver) == address(0)) {
+            revert InvalidRole();
+        }
+
+        return resolver.check(attestation.attester);
+    }
+
+    function onRevoke(
+        Attestation calldata,
+        uint256
+    ) internal virtual override returns (bool) {
         return true;
     }
 }
--- contracts/src/resolvers/DelegatorResolver.sol
+++ contracts/src/resolvers/DelegatorResolver.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.26;
 import "./ICustomResolver.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract DelegateResolver is ICustomResolver {
+contract DelegatorResolver is ICustomResolver {
     ERC20Votes immutable opToken =
         ERC20Votes(0x4200000000000000000000000000000000000042);
 
     function check(address addr) external view override returns (bool) {
-        return opToken.getVotes(addr) > 0;
+        return opToken.delegates(addr) != address(0);
     }
 }
