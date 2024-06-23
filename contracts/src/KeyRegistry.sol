c980a77281933c344e3d0502d21c72a21a6762b0
Pakorn Nathong <pakorn.nth@hotmail.com>
2024-06-23 23:12:08 +0700

    feat(contract): use query function for key

--- contracts/src/KeyRegistry.sol
+++ contracts/src/KeyRegistry.sol
@@ -3,11 +3,22 @@ pragma solidity ^0.8.26;
 
 // Registry of EDDSA public keys
 contract KeyRegistry {
+    struct Key {
+        bytes32 x;
+        bytes32 y;
+    }
     // address -> [x, y]
-    mapping(address => bytes32[2]) public keys;
+    mapping(address => Key) keys;
+
+    event KeyRegistered(address indexed addr, bytes32 x, bytes32 y);
+
+    function key(address addr) external view returns (Key memory) {
+        return keys[addr];
+    }
 
     // Register a public key associated with the caller's address
-    function registerKey(bytes32[2] memory key) public {
-        keys[msg.sender] = key;
+    function registerKey(Key memory k) external {
+        keys[msg.sender] = k;
+        emit KeyRegistered(msg.sender, k.x, k.y);
     }
 }
